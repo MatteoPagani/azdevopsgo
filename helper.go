81b165993d330e905b6c670fdc7a5ff0c8bdbf59
Matteo Pagani <matteo.pagani@aquest.it>
2019-10-15 17:20:03 +0200

    Using CLI library

--- helper.go
+++ helper.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
+
 func getProjectByName(project string) Project {
 	for _, element := range projects {
 		if element.Name == project {
@@ -8,4 +16,56 @@ func getProjectByName(project string) Project {
 	}
 
 	return Project{}
+}
+
+func askForProject() {
+	projectNames := getProjectNames()
+	prompt := promptui.Select{
+		Label: "Which project?",
+		Items: projectNames,
+	}
+
+	_, selectedProject, err := prompt.Run()
+
+	project = selectedProject
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	definitionsNames := getProjectDefinitions(project)
+	prompt = promptui.Select{
+		Label: "Which pipeline?",
+		Items: definitionsNames,
+	}
+
+	_, definitionChoosed, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	for _, element := range definitions {
+		if element.Name == definitionChoosed {
+			definitionId = element.Id
+		}
+	}
+}
+
+func initConfig() error {
+
+	file, err := os.Open("config.json")
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"log"
 	"os"
 
-	"github.com/manifoldco/promptui"
+	"github.com/urfave/cli"
 )
 
 var definitions []GeneralStruct
@@ -16,68 +15,30 @@ var configuration Configuration
 var project string
 var definitionId int
 
-func initConfig() error {
-
-	file, err := os.Open("config.json")
-	if err != nil {
-		return err
-	}
-
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&configuration)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func askForProject() {
-	projectNames := getProjectNames()
-	prompt := promptui.Select{
-		Label: "Which project?",
-		Items: projectNames,
-	}
-
-	_, selectedProject, err := prompt.Run()
-
-	project = selectedProject
-
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-
-	definitionsNames := getProjectDefinitions(project)
-	prompt = promptui.Select{
-		Label: "Which pipeline?",
-		Items: definitionsNames,
-	}
-
-	_, definitionChoosed, err := prompt.Run()
-
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-
-	for _, element := range definitions {
-		if element.Name == definitionChoosed {
-			definitionId = element.Id
-		}
-	}
-}
-
 func main() {
 
-	initConfig()
+	app := cli.NewApp()
+	app.Name = "devops"
+	app.Usage = "Get Azure DevOps last builds"
+	app.Action = func(c *cli.Context) error {
 
-	if configuration.Project.Name == "" || configuration.Project.Definition == 0 {
-		askForProject()
-	} else {
-		project = configuration.Project.Name
-		definitionId = configuration.Project.Definition
+		initConfig()
+
+		if configuration.Project.Name == "" || configuration.Project.Definition == 0 {
+			askForProject()
+		} else {
+			project = configuration.Project.Name
+			definitionId = configuration.Project.Definition
+		}
+
+		getBuildsOfDefinition()
+
+		return nil
 	}
 
-	getBuildsOfDefinition()
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
